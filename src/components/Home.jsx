00730efd1964794cb9f39db76a2fcb0ef17f953f
Rohit <rohiitnagar@gmail.com>
2026-04-10 00:56:46 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import PageHeading from "./PageHeading";
+import React from "react";
+import ProductListing from "./ProductListing";
+import products from "../data/products";
+
 export default function Home(){
     return(
         <div className="home-container">
             <PageHeading title="Welcome to Our Store">
                 Discover a world of creativity with our unique stickers.
             </PageHeading>
+            <ProductListing products={products} />
         </div>
     );
 }
